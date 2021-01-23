f79ff0aeb93103883f893a09c83cb2ac6f534f4a
KeyKatyu <keykatyu@gmail.com>
2021-01-23 22:37:02 +0100

    Update MotifDeplacement.java | New reasons.

--- src/main/java/com/github/keykatyu/motif/MotifDeplacement.java
+++ src/main/java/com/github/keykatyu/motif/MotifDeplacement.java
@@ -2,15 +2,14 @@ package com.github.keykatyu.motif;
 
 public enum MotifDeplacement {
 
-    TRAVAIL("Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle ou un établissement d’enseignement ou de formation ; déplacements professionnels ne pouvant être différés ; déplacements pour un concours ou un examen.",486),
-    ACHATS("Déplacements pour se rendre dans un établissement culturel autorisé ou un lieu de culte ; déplacements pour effectuer des achats de biens, pour des services dont la fourniture estautorisée, pour les retraits de commandes et les livraisons à domicile.",416),
-    SANTE("Consultations, examens et soins ne pouvant être assurés à distance et l’achat de médicaments.",370),
-    FAMILLE("Déplacements pour motif familial impérieux, pour l'assistance aux personnes vulnérables et précaires ou la garde d'enfants.",348),
+    TRAVAIL("Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle ou le lieu d’enseignement et de formation, déplacements professionnels ne pouvant être différés.",486),
+    SANTE("Déplacements pour des consultations, examens, actes de prévention (dont vaccination) et soins ne pouvant être assurés à distance ou pour l’achat de produits de santé.",370),
+    FAMILLE("Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou précaires ou pour la garde d’enfants.",348),
     HANDICAP("Déplacement des personnes en situation de handicap et leur accompagnant.",315),
-    SPORT_ANIMAUX("Déplacements en plein air ou vers un lieu de plein air, sans changement du lieu de résidence, dans la limite de trois heures quotidiennes et dans un rayon maximal de vingt kilomètres autour du domicile, liés soit à l’activité physique ou aux loisirs individuels, à l’exclusion de toute pratique sportive collective et de toute proximité avec d’autres personnes, soit à la promenade avec les seules personnes regroupées dans un même domicile, soit aux besoins des animaux de compagnie.",293),
-    CONVOCATION("Convocation judiciaire ou administrative et pour se rendre dans un service public.",211),
-    MISSIONS("Participation à des missions d'intérêt général sur demande de l'autorité administrative.",177),
-    ENFANTS("Déplacement pour chercher les enfants à l’école et à l’occasion de leurs activités périscolaires.",156);
+    CONVOCATION("Déplacements pour répondre à une convocation judiciaire ou administrative.",211),
+    MISSIONS("Déplacements pour participer à des missions d’intérêt général sur demande de l’autorité administrative.",177),
+    TRANSITS("Déplacements liés à des transits ferroviaires, aériens ou en bus pour des déplacements de longues distances.", 0),
+    ANIMAUX("Déplacements brefs, dans un rayon maximal d’un kilomètre autour du domicile pour les besoins des animaux de compagnie.",156);
 
     private final String raison;
     private final int y;
