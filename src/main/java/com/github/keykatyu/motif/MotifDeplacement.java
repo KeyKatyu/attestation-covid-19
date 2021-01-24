9a5953a67bc159a54a7b28011508c3471aadf4ae
KeyKatyu <keykatyu@gmail.com>
2021-01-24 15:26:59 +0100

    v1.1.2

--- src/main/java/com/github/keykatyu/motif/MotifDeplacement.java
+++ src/main/java/com/github/keykatyu/motif/MotifDeplacement.java
@@ -5,9 +5,9 @@ public enum MotifDeplacement {
     TRAVAIL("Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle ou le lieu d’enseignement et de formation, déplacements professionnels ne pouvant être différés.", 449),
     SANTE("Déplacements pour des consultations, examens, actes de prévention (dont vaccination) et soins ne pouvant être assurés à distance ou pour l’achat de produits de santé.", 415),
     FAMILLE("Déplacements pour motif familial impérieux, pour l’assistance aux personnes vulnérables ou précaires ou pour la garde d’enfants.", 381),
-    HANDICAP("Déplacement des personnes en situation de handicap et leur accompagnant.", 349),
-    CONVOCATION("Déplacements pour répondre à une convocation judiciaire ou administrative.", 320),
-    MISSIONS("Déplacements pour participer à des missions d’intérêt général sur demande de l’autorité administrative.", 300),
+    HANDICAP("Déplacement des personnes en situation de handicap et leur accompagnant.", 348),
+    CONVOCATION("Déplacements pour répondre à une convocation judiciaire ou administrative.", 328),
+    MISSIONS("Déplacements pour participer à des missions d’intérêt général sur demande de l’autorité administrative.", 305),
     TRANSITS("Déplacements liés à des transits ferroviaires, aériens ou en bus pour des déplacements de longues distances.", 210),
     ANIMAUX("Déplacements brefs, dans un rayon maximal d’un kilomètre autour du domicile pour les besoins des animaux de compagnie.", 145);
 
